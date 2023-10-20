64db3ffabe591b4d88e2a5a2a044007fd3262063
Mostafa1630 <mstfyabwsdyt@gmail.com>
2023-10-20 16:51:03 +0200

    Created By Mostafa Maher

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#090b0e",
+        secondary: "#1b1c21",
+        textColor: "#646464",
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
